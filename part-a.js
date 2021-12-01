4158d67e0cbac5db86401fffca2c1e91d2fa5089
vBalatroni <valerio@MacBook-Pro-di-Valerio.local>
2021-12-01 18:32:54 +0100

    part a

---
+++ part-a.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+fs.readFile(__dirname + "/numbers.txt", (error, data) => {
+    if(error) {
+        throw error;
+    }
+    //console.log(data.toString());
+    const numbers = (data.toString()).split("\n");
+    //console.log(numbers.length)
+    
+    let increased = 0;
+    let decreased = 0;
+    numbers.forEach((element, i, arr) => {
+        if (i) {
+            if (arr[i] > arr[i-1]) {
+                increased++
+                console.log("from:", arr[i-1], " - to: ", arr[i], " increased " ,i)
+            }
+            else {
+                decreased++
+                console.log("from:", arr[i-1], " - to: ", arr[i], " decreased " ,i)
+
+            }
+        } 
+    });
+
+    console.log(increased, decreased)
+    console.log(increased + decreased)
+});

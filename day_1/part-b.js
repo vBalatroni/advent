e624b081af027904fe1fae011f24cd4758c887a3
vBalatroni <valerio@MacBook-Pro-di-Valerio.local>
2021-12-02 12:17:30 +0100

    gabriele's corrections

--- day_1/part-b.js
+++ day_1/part-b.js
@@ -2,44 +2,37 @@ const fs = require("fs");
 
 
 fs.readFile(__dirname + "/numbers.txt", (error, data) => {
-    if(error) {
+    if (error) {
         throw error;
     }
     //console.log(data.toString());
     const numbers = (data.toString()).split("\n").map(n => parseInt(n));
-    console.log(numbers.length)
-    
-    let increased = 0;
-    let decreased = 0;
+    console.log(numbers.length);
     let count = 0;
     let temp_sum = 0;
-    console.log(temp_sum)
-    let new_arr = (splitToChunks(numbers, 667))
-    new_arr.forEach((element, i, arr) => {
+    console.log(temp_sum);
+    let new_arr = (splitToChunks(numbers, 667));
+    new_arr.forEach(element => {
         let new_sum = 0;
-        element.forEach((e, i, a) => {
-            if (a.length === 3) {
-                console.log(e)
-                new_sum += e
+        element.forEach(e => {
+            if (element.length === 3) {
+                console.log(e);
+                new_sum += e;
             }
-        })
-        console.log(new_sum)
-        console.log("\n")   
+        });
+        console.log(new_sum);
+        console.log("\n");
         if (temp_sum != 0) {
             if (new_sum > temp_sum) {
-            count++
-            console.log("new sum:", new_sum, ' > ', "old_sum: ", temp_sum, "increased", count)
-            }
-            else {
-                console.log("new sum:", new_sum, ' < ', "old_sum: ", temp_sum , "decreased")
+            count++;
+            console.log("new sum:", new_sum, ' > ', "old_sum: ", temp_sum, "increased", count);
+            } else {
+                console.log("new sum:", new_sum, ' < ', "old_sum: ", temp_sum , "decreased");
             }
         }
         temp_sum = new_sum;
-    })
-
-    console.log(count)
-
-
+    });
+    console.log(count);
 });
 
 function splitToChunks(array, parts){
